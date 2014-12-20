fae03660f85367c6c5e58b6115cab3138ee3ce01
Defman21 <thefyro1@gmail.com>
2014-12-21 01:09:53 +0300

    Update name of extension and id

--- content/init.js
+++ content/init.js
@@ -1,14 +1,14 @@
 try {
   var schemeService = Cc['@activestate.com/koScintillaSchemeService;1'].getService();
-  var schemeName = "Spacemodo";
+  var schemeName = "Oceamodo";
   if (schemeService.getScheme(schemeName) != schemeName)
   {
-    var _url = "chrome://spacemodoo/content/" + schemeName + ".ksf";
+    var _url = "chrome://oceamodo/content/" + schemeName + ".ksf";
     var result = schemeService.loadSchemeFromURI(_url, schemeName);
   }
   var oldScheme = schemeService.activateScheme(schemeName);
   ko.open._notifyHowtoRestoreOldScheme(schemeService, oldScheme, schemeName);
   ko.prefs.setBoolean("showFileIcons", true);
 } catch (e) {
-  alert("Unable to set Spacemodo scheme! You should do that manually :)");
+  alert("Unable to set Oceamodo scheme! You should do that manually :)");
 }
